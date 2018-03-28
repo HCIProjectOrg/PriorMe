bcd31643038ff087aefcd6d7c8ee3dcf3ddadd4e
Brett Benda <brett.benda@ufl.edu>
2018-03-28 15:25:02 -0400

    Tasks are displayed using data stored in array
    No longer directly displayed from direct queries

--- www/js/tasks.js
+++ www/js/tasks.js
@@ -54,44 +54,72 @@ app.initialize();
 /**
  * Starts listening for new posts and populates posts lists.
  */
-function startDatabaseQueries() {
+ function startDatabaseQueries() {
     var tasknumber = 0; 
+    var taskKeys = [];
+    var taskNames = [];
+    var taskDeadlines = [];
+    var taskDetails = [];
+    var taskType = [];
+    var taskHours = [];
+
 
     var query = firebase.database().ref("Task").orderByKey();
-    query.once("value").then(function(snapshot) {
+
+    query.once("value").then(
+
+    function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
+            console.log("TASK");
+            taskKeys.push(childSnapshot.key);
+            taskNames.push(childSnapshot.val().Name);
+            taskDeadlines.push(childSnapshot.val().Deadline);
+            taskDetails.push(childSnapshot.val().Details);
+            taskHours.push(childSnapshot.val().Hours);
+            taskType.push(childSnapshot.val().TaskType);
+            console.log(taskKeys[0]);
+            console.log(taskNames[0]);
+            console.log(taskDeadlines[0]);
+            console.log(taskDetails[0]);
+            console.log(taskHours[0]);
+            console.log(taskType[0]);
+        });
+    }).then(
+
+    function(){
+        for(var i=0; i<taskKeys.length; i++){
+            console.log("CARD");
             tasknumber++;
-            var key = childSnapshot.key;
+            var key = taskKeys[i];
 
             var pnl = document.getElementById("mainDiv");
             var taskDiv = document.createElement("div");
             taskDiv.id = key;
 
-             var keyString = key.toString();
-             var popUpKey = "P"+keyString;
-             console.log("popUpKey: " + popUpKey);
-            // childSnapshot.val().Hours
-            // taskDiv.innerHTML = tasknumber + " " + childSnapshot.key +  " Due Date: " + childSnapshot.val().Deadline ;
+            var keyString = key.toString();
+            var popUpKey = "P"+keyString;
+            console.log("popUpKey: " + popUpKey);
 
             var html = 
             '<div class="task" onclick=myFunction(\'' + popUpKey + '\')>'+ 
-                    '<div class="popuptext" id='+popUpKey+'>' + 
-                        '<div class="popupName" >' + 
-                            '<p class="namePopUp">' + childSnapshot.key +'</p>' +
-                        '</div>'+
-                        '<p class="deadlinePopUp">' + "Due: "+ childSnapshot.val().Deadline  +'</p>' +
-                        '<p class="detailsPopUp">' + "Details: "+ childSnapshot.val().Details  +'</p>' +
-                    '</div>'+
-                    '<label class="tasknumberLabel">' + tasknumber + " "+ '</label>' +
-                    '<label class="nameLabel">' + childSnapshot.key  +'</label>' +
-                    '</br>'+
-                    '<label class="deadlineLabel">' + "Due: "+ childSnapshot.val().Deadline  +'</label>' +
+            '<div class="popuptext" id='+popUpKey+'>' + 
+            '<div class="popupName" >' + 
+            '<p class="namePopUp">' + taskNames[i] +'</p>' +
+            '</div>'+
+            '<p class="deadlinePopUp">' + "Due: "+ taskDeadlines[i]  +'</p>' +
+            '<p class="detailsPopUp">' + "Details: "+ taskDetails[i]  +'</p>' +
+            '</div>'+
+            '<label class="tasknumberLabel">' + tasknumber + " "+ '</label>' +
+            '<label class="nameLabel">' + taskNames[i]  +'</label>' +
+            '</br>'+
+            '<label class="deadlineLabel">' + "Due: "+ taskDeadlines[i]  +'</label>' +
             '</div>';
 
             taskDiv.innerHTML = html;
             pnl.appendChild(taskDiv);
-        });
+        }
     });
+
 }  
 
 
