aee29bb99cd06012003e5a9a938226e8b1c3664d
Brett Benda <brett.benda@ufl.edu>
2018-03-28 15:56:24 -0400

    Tasks are sorted and displayed according to the user's settings
    No change over time yet

--- www/js/tasks.js
+++ www/js/tasks.js
@@ -62,64 +62,134 @@ app.initialize();
     var taskDetails = [];
     var taskType = [];
     var taskHours = [];
+    var taskPriorities =[];
+
+    var examPriority = 0;
+    var homeworkPriority = 0;
+    var projectPriority = 0;
+    var researchPriority = 0;
 
 
     var query = firebase.database().ref("Task").orderByKey();
 
     query.once("value").then(
 
+    //store task data from DB
     function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
-            console.log("TASK");
+            //console.log("TASK");
             taskKeys.push(childSnapshot.key);
             taskNames.push(childSnapshot.val().Name);
             taskDeadlines.push(childSnapshot.val().Deadline);
             taskDetails.push(childSnapshot.val().Details);
             taskHours.push(childSnapshot.val().Hours);
             taskType.push(childSnapshot.val().TaskType);
-            console.log(taskKeys[0]);
-            console.log(taskNames[0]);
-            console.log(taskDeadlines[0]);
-            console.log(taskDetails[0]);
-            console.log(taskHours[0]);
-            console.log(taskType[0]);
+            //console.log(taskKeys[0]);
+            //console.log(taskNames[0]);
+            //console.log(taskDeadlines[0]);
+            //console.log(taskDetails[0]);
+            //console.log(taskHours[0]);
+            //console.log(taskType[0]);
         });
     }).then(
 
+    //get priority data from DB
     function(){
-        for(var i=0; i<taskKeys.length; i++){
-            console.log("CARD");
-            tasknumber++;
-            var key = taskKeys[i];
+        var query2 = firebase.database().ref("Priorities");
+        query2.once("value").then(
 
-            var pnl = document.getElementById("mainDiv");
-            var taskDiv = document.createElement("div");
-            taskDiv.id = key;
+            function(snapshot){
+                examPriority = snapshot.val().examPriority;
+                homeworkPriority = snapshot.val().homeworkPrioirity;
+                projectPriority = snapshot.val().projectPriority;
+                researchPriority = snapshot.val().researchPriority;
+            }
 
-            var keyString = key.toString();
-            var popUpKey = "P"+keyString;
-            console.log("popUpKey: " + popUpKey);
+            ).then(
 
-            var html = 
-            '<div class="task" onclick=myFunction(\'' + popUpKey + '\')>'+ 
-            '<div class="popuptext" id='+popUpKey+'>' + 
-            '<div class="popupName" >' + 
-            '<p class="namePopUp">' + taskNames[i] +'</p>' +
-            '</div>'+
-            '<p class="deadlinePopUp">' + "Due: "+ taskDeadlines[i]  +'</p>' +
-            '<p class="detailsPopUp">' + "Details: "+ taskDetails[i]  +'</p>' +
-            '</div>'+
-            '<label class="tasknumberLabel">' + tasknumber + " "+ '</label>' +
-            '<label class="nameLabel">' + taskNames[i]  +'</label>' +
-            '</br>'+
-            '<label class="deadlineLabel">' + "Due: "+ taskDeadlines[i]  +'</label>' +
-            '</div>';
+            //sort tasks then displayed based on ordering
+            function(){
+                //associate priorities with given categories
+                for(var i=0; i<taskKeys.length; i++){
+                    if(taskType[i]=="exam"){
+                        taskPriorities.push(examPriority);
+                    }else if(taskType[i]=="homework"){
+                        taskPriorities.push(homeworkPriority);
+                    }else if(taskType[i]=="project"){
+                        taskPriorities.push(projectPriority);
+                    }else if(taskType[i]=="research"){
+                        taskPriorities.push(researchPriority);
+                    }else{
+                        taskPriorities.push(5);
+                    }
+                }
 
-            taskDiv.innerHTML = html;
-            pnl.appendChild(taskDiv);
-        }
+                for(var i=0; i<taskPriorities.length; i++){
+                    for(var j = 0; j<taskPriorities.length; j++){
+                        if(taskPriorities[i]>taskPriorities[j]){
+                            var temp = taskDeadlines[i];
+                            taskDeadlines[i] = taskDeadlines[j];
+                            taskDeadlines[j] = temp;
+
+                            temp = taskKeys[i];
+                            taskKeys[i] = taskKeys[j];
+                            taskKeys[j] = temp;
+
+                            temp = taskHours[i];
+                            taskHours[i] = taskHours[j];
+                            taskHours[j] = temp;
+
+                            temp = taskNames[i];
+                            taskNames[i] = taskNames[j];
+                            taskNames[j] = temp;
+
+                            temp = taskDetails[i];
+                            taskDetails[i] = taskDetails[j];
+                            taskDetails[j] = temp;
+
+                            temp = taskType[i];
+                            taskType[i] = taskType[j];
+                            taskType[j] = temp;
+
+                            temp = taskPriorities[i];
+                            taskPriorities[i] = taskPriorities[j];
+                            taskPriorities[j] = temp;
+                        }
+                    }
+                }
+                
+                //display
+                for(var i=0; i<taskKeys.length; i++){
+                    tasknumber++;
+                    var key = taskKeys[i];
+
+                    var pnl = document.getElementById("mainDiv");
+                    var taskDiv = document.createElement("div");
+                    taskDiv.id = key;
+
+                    var keyString = key.toString();
+                    var popUpKey = "P"+keyString;
+
+                    var html = 
+                    '<div class="task" onclick=myFunction(\'' + popUpKey + '\')>'+ 
+                    '<div class="popuptext" id='+popUpKey+'>' + 
+                    '<div class="popupName" >' + 
+                    '<p class="namePopUp">' + taskNames[i] +'</p>' +
+                    '</div>'+
+                    '<p class="deadlinePopUp">' + "Due: "+ taskDeadlines[i]  +'</p>' +
+                    '<p class="detailsPopUp">' + "Details: "+ taskDetails[i]  +'</p>' +
+                    '</div>'+
+                    '<label class="tasknumberLabel">' + tasknumber + " "+ '</label>' +
+                    '<label class="nameLabel">' + taskNames[i]  +'</label>' +
+                    '</br>'+
+                    '<label class="deadlineLabel">' + "Due: "+ taskDeadlines[i]  +'</label>' +
+                    '</div>';
+
+                    taskDiv.innerHTML = html;
+                    pnl.appendChild(taskDiv);
+                }
+            });
     });
-
 }  
 
 
